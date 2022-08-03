7a370e51765711e5781a1bd41b136afd97ebb253
Albert Brown <103575236+Albert-7317@users.noreply.github.com>
2022-08-03 08:32:49 +0000

    Add files via upload

---
+++ fish.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+class Wall {
+    constructor(x, y, width, height) {
+        this.xy {
+            x: x;
+        }
+        this.width = width
+        this.height = height
+    }
+}
+
+
